2710e26c416a985862e5c46b54b744d6252ec139
MegaMix_Craft <43725099+minusQuantumNeko@users.noreply.github.com>
2025-03-24 14:23:25 +0200

    Update code to properly link avatars
    The file name is just username now

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ for (let i = 0; i < parts.length; i++) {
     if(question_or_answer == 0) {
 
         if (interviewerAvatarVersion > 1) {
-            formattedInterviewText += `<a class="avatar news-chat-quote__avatar" href="https://osu.ppy.sh/users/${interviewerID}" style="background-image: url('/wiki/shared/avatars/avatar-${interviewerName.replace(" ", "_")}-${interviewerAvatarVersion}.jpg')"></a>\n\n<p class="news-chat-quote__username"><a class="news-chat-quote__colour-${interviewerGroup}" href="https://osu.ppy.sh/users/${interviewerID}">${interviewerName}</a></p>\n\n${parts[i].replace(/\*\*\*/mg, "")}\n\n`
+            formattedInterviewText += `<a class="avatar news-chat-quote__avatar" href="https://osu.ppy.sh/users/${interviewerID}" style="background-image: url('/wiki/shared/avatars/${interviewerName.replace(" ", "_")}-${interviewerAvatarVersion}.jpg')"></a>\n\n<p class="news-chat-quote__username"><a class="news-chat-quote__colour-${interviewerGroup}" href="https://osu.ppy.sh/users/${interviewerID}">${interviewerName}</a></p>\n\n${parts[i].replace(/\*\*\*/mg, "")}\n\n`
         } else {
-            formattedInterviewText += `<a class="avatar news-chat-quote__avatar" href="https://osu.ppy.sh/users/${interviewerID}" style="background-image: url('/wiki/shared/avatars/avatar-${interviewerName.replace(" ", "_")}.jpg')"></a>\n\n<p class="news-chat-quote__username"><a class="news-chat-quote__colour-${interviewerGroup}" href="https://osu.ppy.sh/users/${interviewerID}">${interviewerName}</a></p>\n\n${parts[i].replace(/\*\*\*/mg, "")}\n\n`
+            formattedInterviewText += `<a class="avatar news-chat-quote__avatar" href="https://osu.ppy.sh/users/${interviewerID}" style="background-image: url('/wiki/shared/avatars/${interviewerName.replace(" ", "_")}.jpg')"></a>\n\n<p class="news-chat-quote__username"><a class="news-chat-quote__colour-${interviewerGroup}" href="https://osu.ppy.sh/users/${interviewerID}">${interviewerName}</a></p>\n\n${parts[i].replace(/\*\*\*/mg, "")}\n\n`
         }
         question_or_answer = 1
 
     } else if(question_or_answer == 1) {
 
         if (intervieweeAvatarVersion > 1) {
-            formattedInterviewText += `<a class="avatar news-chat-quote__avatar" href="https://osu.ppy.sh/users/${intervieweeID}" style="background-image: url('/wiki/shared/avatars/avatar-${intervieweeName.replace(" ", "_")}-${intervieweeAvatarVersion}.jpg')"></a>\n\n<p class="news-chat-quote__username"><a class="news-chat-quote__colour-${intervieweeGroup}" href="https://osu.ppy.sh/users/${intervieweeID}">${intervieweeName}</a></p>${parts[i]}`
+            formattedInterviewText += `<a class="avatar news-chat-quote__avatar" href="https://osu.ppy.sh/users/${intervieweeID}" style="background-image: url('/wiki/shared/avatars/${intervieweeName.replace(" ", "_")}-${intervieweeAvatarVersion}.jpg')"></a>\n\n<p class="news-chat-quote__username"><a class="news-chat-quote__colour-${intervieweeGroup}" href="https://osu.ppy.sh/users/${intervieweeID}">${intervieweeName}</a></p>${parts[i]}`
         } else {
-            formattedInterviewText += `<a class="avatar news-chat-quote__avatar" href="https://osu.ppy.sh/users/${intervieweeID}" style="background-image: url('/wiki/shared/avatars/avatar-${intervieweeName.replace(" ", "_")}.jpg')"></a>\n\n<p class="news-chat-quote__username"><a class="news-chat-quote__colour-${intervieweeGroup}" href="https://osu.ppy.sh/users/${intervieweeID}">${intervieweeName}</a></p>${parts[i]}`
+            formattedInterviewText += `<a class="avatar news-chat-quote__avatar" href="https://osu.ppy.sh/users/${intervieweeID}" style="background-image: url('/wiki/shared/avatars/${intervieweeName.replace(" ", "_")}.jpg')"></a>\n\n<p class="news-chat-quote__username"><a class="news-chat-quote__colour-${intervieweeGroup}" href="https://osu.ppy.sh/users/${intervieweeID}">${intervieweeName}</a></p>${parts[i]}`
         }
         question_or_answer = 0
 
